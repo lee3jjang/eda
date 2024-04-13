39619ba09928d66a9fa56d6706e04a7537f19d08
Lee Sang Jin <36585973+lee3jjang@users.noreply.github.com>
2024-04-13 17:11:55 +0900

    Add likes (#6)

--- admin/consumer.py
+++ admin/consumer.py
@@ -1,4 +1,15 @@
+import os
+import json
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+
+import django
+
+django.setup()
+
 import pika
+
+from products.models import Product
 
 params = pika.URLParameters(
     "amqps://hjrwcpkr:hTiyvkypUcYq-1FicLwDl054cO7BE1xs@dingo.rmq.cloudamqp.com/hjrwcpkr"
@@ -13,7 +24,12 @@ channel.queue_declare(queue="admin")
 
 def callback(ch, method, properties, body):
     print("Received in admin")
-    print(body)
+    id = json.loads(body)
+    print("id:", id)
+    product = Product.objects.get(id=int(id))
+    product.likes = product.likes + 1
+    product.save()
+    print("Product likes increased!")
 
 
 channel.basic_consume(queue="admin", on_message_callback=callback, auto_ack=True)
