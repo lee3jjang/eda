8f558e622efe1c3b5ed732b1d7890bdf64c3d03d
SangJin Lee <lee3jjang@gmail.com>
2024-04-13 13:27:05 +0900

    Update admin/products

--- admin/products/models.py
+++ admin/products/models.py
@@ -1,9 +1,11 @@
 from django.db.models import CharField, PositiveIntegerField, Model
 
+
 class Product(Model):
     title = CharField(max_length=200)
     image = CharField(max_length=200)
     likes = PositiveIntegerField(default=0)
 
+
 class User(Model):
-    pass 
+    pass
