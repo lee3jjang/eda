9c096d7be2b78d12c3b2fab5271dbb825e569372
SangJin Lee <lee3jjang@gmail.com>
2024-04-13 16:43:07 +0900

    Refactor producer/consumer and view

--- admin/products/views.py
+++ admin/products/views.py
@@ -19,13 +19,13 @@ class ProductViewSet(ViewSet):
     def list(self, request: Request) -> Response:
         product = Product.objects.all()
         serializer = ProductSerializer(product, many=True)
-        publish()
         return Response(serializer.data)
 
     def create(self, request: Request) -> Response:
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish("product_created", serializer.data)
         return Response(serializer.data, status=HTTP_201_CREATED)
 
     def retrieve(self, request: Request, pk: str) -> Response:
@@ -37,12 +37,14 @@ class ProductViewSet(ViewSet):
         product = Product.objects.get(pk=pk)
         serializer = ProductSerializer(instance=product, data=request.data)
         serializer.is_valid(raise_exception=True)
+        publish("product_updated", serializer.data)
         serializer.save()
         return Response(serializer.data, status=HTTP_202_ACCEPTED)
 
     def destroy(self, request: Request, pk: str) -> Response:
         product = Product.objects.get(pk=pk)
         product.delete()
+        publish("product_deleted", pk)
         return Response(status=HTTP_204_NO_CONTENT)
 
 
--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,8 @@
+import json
+
 import pika
+
+from app import app, Product, db
 
 params = pika.URLParameters(
     "amqps://hjrwcpkr:hTiyvkypUcYq-1FicLwDl054cO7BE1xs@dingo.rmq.cloudamqp.com/hjrwcpkr"
@@ -13,10 +17,27 @@ channel.queue_declare(queue="main")
 
 def callback(ch, method, properties, body):
     print("Received in main")
-    print(body)
+    data = json.loads(body)
+    print(data)
+    with app.app_context():
+        if properties.content_type == "product_created":
+            product = Product(id=data["id"], title=data["title"], image=data["image"])
+            db.session.add(product)
+            db.session.commit()
+
+        if properties.content_type == "product_updated":
+            product: Product = Product.query.get(data["id"])
+            product.title = data["title"]
+            product.image = data["image"]
+            db.session.commit()
+
+        if properties.content_type == "product_deleted":
+            product: Product = Product.query.get(data)
+            db.session.delete(product)
+            db.session.commit()
 
 
-channel.basic_consume(queue="main", on_message_callback=callback)
+channel.basic_consume(queue="main", on_message_callback=callback, auto_ack=True)
 
 print("Start Consuming")
 
